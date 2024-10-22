6fb3bf094773196553dd2757477f4d17b370935f
michella <michella.milecka@gmail.com>
2024-10-22 20:22:44 +0200

    new-file

--- data_generator.py
+++ data_generator.py
@@ -9,4 +9,35 @@ chief_names=[]
 for i in range(1,number_of_stations+1):
     chief_names.append((i, fake.name()))
 
-print(chief_names)
+possible_types_of_zdarzenia = [
+    "theft", 
+    "kidnapping", 
+    "disappearance", 
+    "murder", 
+    "assault", 
+    "domestic violence", 
+    "rape", 
+    "manslaughter", 
+    "threatening", 
+    "burglary", 
+    "fraud", 
+    "money laundering", 
+    "tax evasion", 
+    "identity theft"
+]
+
+possible_ways_of_zdarzenie=["personally", "by phone"]
+
+possible_places_of_przesluchanie=["police station", "the house of the interrogated person","area"]
+
+possible_purpose_of_hearing=["determining the course of events", "confirming the words of another witness", "alibi"]
+
+possible_status_of_sledztwo=["in progress","closed-unresolved","closed-resolved","open-resolved"]
+
+possible_type_od_material=["testimony","biological","material","document"]
+
+possible_priorities=["high","medium","low"]
+
+possible_outcome_of_verification=["positive","negative","ambiguous"]
+
+possible_way_of_verification=["identity confirmation","confirmation of the existence of the place indicated by reporting person"]
