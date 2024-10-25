d0511d8a4fd95128547b71446ff0867acce693a9
michella <michella.milecka@gmail.com>
2024-10-25 23:27:29 +0200

    another functions done

--- data_generator.py
+++ data_generator.py
@@ -18,7 +18,6 @@ possible_types_of_zdarzenia = [
     "assault", 
     "domestic violence", 
     "rape", 
-    "manslaughter", 
     "threatening", 
     "burglary", 
     "fraud", 
@@ -90,10 +89,55 @@ def generate_zgloszenia_data(num_of_zgloszenia,zdarzenia_data,analizy_tablica):
         zgloszenie_data = fake.date_between(start_date=data_z, end_date=end_date)
         #id_osoby
         #id_posterunku
-        #numer_odznaki
+        numer_odznaki=random.randint(10000, 99999)
         id_analizy=random.randint(0,len(analizy_tablica)-1)
         zgloszenia_tablica.append((zgloszenie_id,zgloszenie_sposob,zgloszenie_data,id_zdarzenia,id_analizy))
     return zgloszenia_tablica
+
+
+def generate_sledztwo_data(num_of_sledztwa):
+    sledztwa_tablica=[]
+    for i in range(sledztwa_tablica):
+        numer_sledztwa=i
+        #data_ropoczecia
+        #data_zakoczenia
+        status_sledztwa=random.choice(possible_status_of_sledztwo)
+        numer_odznaki=random.randint(10000, 99999)
+        sledztwa_tablica.append((numer_sledztwa,status_sledztwa,numer_odznaki))
+    return sledztwa_tablica
+
+def generate_czynnosc_data(num_of_czynnosci):
+    czynnosci_tablica = []
+    for i in range(num_of_czynnosci):
+        id_czynnosci = i
+        #data = 
+        numerOdznaki = random.randint(10000, 99999)
+        czynnosci_tablica.append(id_czynnosci,numerOdznaki)
+    return czynnosci_tablica
+
+def generate_przesluchanie_data(num_przesluchanie,czynnosci_tablica):
+    przesluchania_tablica=[]
+    for i in range(przesluchania_tablica):
+        #id_czynnosci
+        godzina_przesluchania=fake.time()
+        lokalizacja_przesluchania=random.choice(possible_places_of_przesluchanie)
+        cel_przeslchania=random.choice(possible_purpose_of_hearing)
+        #id_osoby
+        przesluchania_tablica.append(godzina_przesluchania,lokalizacja_przesluchania,cel_przeslchania)
+    return przesluchania_tablica
+
+def generate_weryfikacjaInformacji_data(num_of_weryfikacjaInformacji, czynnosci_tablica):
+    weryfikacjaInformacji_tablica = []
+    for i in range(num_of_weryfikacjaInformacji):
+        # id_weryfikacjiInformacji = tu trzeba bedzie wybierac liczbe czynnosci i potem rozdzielic to dobrze na pozostale encje wchodzace w sklad czynnosc
+        # a pozniej bedziemy generowac np 1. przesluhcanie i tam bedziemy wyrzucac z tablicy juz te id ktore zostaly wykrozystane i przekazywac do weryfikacji informacji itd 
+        priorytet = random.choice(possible_priorities)
+        # opis ?????
+        wynik = random.choice(possible_outcome_of_verification)
+        rodzaj = random.choice(possible_way_of_verification)
+        weryfikacjaInformacji_tablica.append(id_weryfikacjiInformacji, priorytet, opis, wynik, rodzaj)
+    return weryfikacjaInformacji_tablica
+
 
 zdarzenia=generate_zdarzenie_data_later(10)
 analizy=generate_analiza_data(10)
